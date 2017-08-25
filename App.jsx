049d17c39fa56dc461039e6a21de80f8adc6e215
Henry Sloan <henry@linux.suse>
2017-08-25 11:29:30 -0400

    Streamline design and component model and add skills information

--- App.jsx
+++ App.jsx
@@ -6,8 +6,11 @@ class ContactInfo extends React.Component {
   render() {
     return (
       <div id='contact-info'>
+        <i className='fa fa-location-arrow text-icon' />
         <p>{Resume.basics.location.region}, {Resume.basics.location.countryCode}</p>
-        <a href={"mailto:" + Resume.basics.email}>{Resume.basics.email}</a>
+        <br />
+        <i className='fa fa-envelope text-icon' />
+        <a href={'mailto:' + Resume.basics.email}>{Resume.basics.email}</a>
       </div>
     );
   }
@@ -18,7 +21,7 @@ class ProfileLinks extends React.Component {
     return (
       <ul className='profiles'>
         <li>
-          <a className="fa fa-github" href={Resume.basics.profiles[0].url}></a>
+          <a className='fa fa-github' href={Resume.basics.profiles[0].url} />
         </li>
       </ul>
     );
@@ -45,15 +48,65 @@ class Sidebar extends React.Component {
   }
 }
 
+class Section extends React.Component {
+  render() {
+    var icon = this.props.icon;
+    if (icon) {
+      icon = <i className={'fa ' + icon + ' text-icon'} />;
+    }
+    else {
+      icon = null;
+    }
+    return (
+      <div className='section'>
+        <h1>
+          {icon}
+          {this.props.name}
+        </h1>
+        <hr />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+class AboutSection extends React.Component {
+  render() {
+    return (
+      <Section name='About' icon='fa-user'>
+        <p>{Resume.basics.summary}</p>
+      </Section>
+    );
+  }
+}
+
+class SkillsSection extends React.Component {
+  render() {
+    var skills = [];
+    Resume.skills.forEach(function(skill, index) {
+      skills.push(
+        <div className='skill' key={index}>
+          <h3>{skill.name}</h3>
+          <p>{skill.keywords.join(', ')}</p>
+        </div>);
+    });
+
+    return (
+      <Section name='Skills' icon='fa-cog'>
+        {skills}
+      </Section>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
       <div>
         <Sidebar />
-        <div id="main">
-          <h1>About</h1>
-          <hr />
-          <p>{Resume.basics.summary}</p>
+        <div id='main'>
+          <AboutSection />
+          <SkillsSection />
         </div>
       </div>);
   }
